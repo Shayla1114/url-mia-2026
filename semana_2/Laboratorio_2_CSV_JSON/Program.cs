39187d89f60eaa01c10a45fb93a46a4fc75f6280
Shayla De Leon <smdeleonmon@correo.url.edu.gt>
2026-07-19 22:37:33 -0600

    Agregar laboratorio de semana 2

---
+++ semana_2/Laboratorio_2_CSV_JSON/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+string[] lineas = File.ReadAllLines("estudiantes.csv");
+List<Estudiante> estudiantes = new List<Estudiante>();
+
+for (int i =1; i < lineas.Length; i++)
+{
+    string[] datos = lineas[i].Split(',');
+
+    Estudiante estudiante = new Estudiante
+    {
+        Id = int.Parse(datos[0]),
+        Nombre = datos[1],
+        Carrera = datos[2]
+    };
+
+    estudiantes.Add(estudiante);
+    Console.WriteLine(estudiante.Id+ " - " + estudiante.Nombre + " - " + estudiante.Carrera);
+}
+
+string json = JsonSerializer.Serialize(estudiantes);
+File.WriteAllText("estudiantes.json", json);
+Console.WriteLine("Archivo estudiantes.json creado correctamente.");
