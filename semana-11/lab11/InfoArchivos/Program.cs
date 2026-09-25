be82b485c4870c5fbb6ef2d948627440b24ebc40
Shayla De Leon <smdeleonmon@correo.url.edu.gt>
2026-09-25 10:31:39 -0600

    Agregar proyecto laboratorio semana 11

---
+++ semana-11/lab11/InfoArchivos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("INFORMACION DE ARCHIVOS Y DIRECTORIOS");
+        Console.WriteLine();
+
+        Console.Write("Ingrese una ruta: ");
+        string ruta = Console.ReadLine();
+
+        Console.WriteLine();
+
+        if (File.Exists(ruta))
+        {
+            Console.WriteLine("La ruta corresponde a un archivo.");
+
+            FileInfo archivo = new FileInfo(ruta);
+
+            Console.WriteLine("Nombre: " + archivo.Name);
+            Console.WriteLine("Tamaño: " + archivo.Length + " bytes");
+            Console.WriteLine("Ultima modificacion: " + archivo.LastWriteTime);
+            Console.WriteLine("Atributos: " + archivo.Attributes);
+        }
+        else if (Directory.Exists(ruta))
+        {
+            Console.WriteLine("La ruta corresponde a un directorio.");
+            Console.WriteLine();
+
+            string[] archivos = Directory.GetFiles(ruta);
+
+            Console.WriteLine("Archivos encontrados:");
+
+            foreach (string archivoRuta in archivos)
+            {
+                FileInfo archivo = new FileInfo(archivoRuta);
+
+                Console.WriteLine();
+                Console.WriteLine("Nombre: " + archivo.Name);
+                Console.WriteLine("Tamaño: " + archivo.Length + " bytes");
+                Console.WriteLine("Ultima modificacion: " + archivo.LastWriteTime);
+                Console.WriteLine("Atributos: " + archivo.Attributes);
+            }
+        }
+        else
+        {
+            Console.WriteLine("La ruta ingresada no existe.");
+        }
+    }
+}
